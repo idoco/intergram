09c266979dfcc593724300e991b719b56e39b8f9
idoco <ido.c.mail@gmail.com>
2016-12-25 17:31:22 +0200

    styling

---
+++ embed/intergram.js
@@ -0,0 +1,38 @@
+
+(function () {
+
+    function onClick() {
+        if (!window.intergramId) {
+            console.error("To use intergram you have to set an intergramId ");
+            return;
+        }
+
+        document.querySelector('#intergramTitle').style.display = 'none';
+        document.querySelector('#intergramRoot').
+        insertAdjacentHTML('beforeend',
+            '<iframe src=\'https:\/\/intergram.herokuapp.com\/#' + window.intergramId +
+            '\' width=\'300\' height=\'350\' frameborder=\'0\'><\/iframe>')
+    }
+
+    // add a style tag to the head
+    var styleTag = document.createElement("link");
+    styleTag.rel = "stylesheet";
+    styleTag.type = "text/css";
+    styleTag.href = "embed/style.css";
+    styleTag.media = "all";
+    document.getElementsByTagName('head')[0].appendChild(styleTag);
+
+    // create the wrapper
+    var root = document.createElement('div');
+    root.id = 'intergramRoot';
+    document.getElementsByTagName('body')[0].appendChild(root);
+
+    // create the inner div
+    var div = document.createElement('div');
+    div.id = 'intergramTitle';
+    div.onclick = onClick;
+    root.appendChild(div);
+
+    div.innerHTML = "Click to chat with us!"
+
+})();
