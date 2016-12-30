b75b84a7032faddf3775a49076b426d5087dbec8
idoco <ido.c.mail@gmail.com>
2016-12-30 15:55:22 +0200

    make chat closeable

--- embed/inject.js
+++ embed/inject.js
@@ -1,38 +1,59 @@
 
 (function () {
 
-    function onClick() {
+    injectCSS();
+    if (window.attachEvent) {
+        window.attachEvent('onload', injectChat);
+    } else {
+        window.addEventListener('load', injectChat, false);
+    }
+
+    function openClick() {
         if (!window.intergramId) {
-            console.error("To use intergram you have to set an intergramId ");
+            console.error("To use Intergram you have to set an intergramId ");
             return;
         }
 
-        document.querySelector('#intergramTitle').style.display = 'none';
+        document.querySelector('#intergramTitle').innerHTML = 'Intergram';
+        document.querySelector('#intergramTitle').onclick = closeClick;
+
         document.querySelector('#intergramRoot').
         insertAdjacentHTML('beforeend',
             '<iframe src=\'https:\/\/idoco.github.io/intergram/chat.html#' + window.intergramId +
-            '\' width=\'300\' height=\'350\' frameborder=\'0\'><\/iframe>')
+            '\' width=\'300\' height=\'350\' frameborder=\'0\' id=\'intergramIframe\' ><\/iframe>')
     }
 
-    // add a style tag to the head
-    var styleTag = document.createElement("link");
-    styleTag.rel = "stylesheet";
-    styleTag.type = "text/css";
-    styleTag.href = "https://idoco.github.io/intergram/embed/style.css";
-    styleTag.media = "all";
-    document.getElementsByTagName('head')[0].appendChild(styleTag);
+    function closeClick() {
+        document.querySelector('#intergramTitle').innerHTML = "Click to chat with us!";
+        document.querySelector('#intergramTitle').onclick = openClick;
 
-    // create the wrapper
-    var root = document.createElement('div');
-    root.id = 'intergramRoot';
-    document.getElementsByTagName('body')[0].appendChild(root);
+        var chatIframe = document.querySelector('#intergramIframe');
+        chatIframe.parentNode.removeChild(chatIframe);
+    }
 
-    // create the inner div
-    var div = document.createElement('div');
-    div.id = 'intergramTitle';
-    div.onclick = onClick;
-    root.appendChild(div);
+    function injectCSS() {
+        // add a style tag to the head
+        var styleTag = document.createElement("link");
+        styleTag.rel = "stylesheet";
+        styleTag.type = "text/css";
+        styleTag.href = "https://idoco.github.io/intergram/embed/style.css";
+        styleTag.media = "all";
+        document.getElementsByTagName('head')[0].appendChild(styleTag);
+    }
 
-    div.innerHTML = "Click to chat with us!"
+    function injectChat() {
+        // create the wrapper
+        var root = document.createElement('div');
+        root.id = 'intergramRoot';
+        document.getElementsByTagName('body')[0].appendChild(root);
+
+        // create the inner div
+        var div = document.createElement('div');
+        div.id = 'intergramTitle';
+        div.onclick = openClick;
+        root.appendChild(div);
+
+        div.innerHTML = "Click to chat with us!"
+    }
 
 })();
