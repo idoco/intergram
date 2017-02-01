3286fb229ba901474837db851078d2dc368f7c42
idoco <ido.c.mail@gmail.com>
2017-02-01 11:14:23 +0200

    persist userId

--- src/chat/chat-index.js
+++ src/chat/chat-index.js
@@ -1,14 +1,25 @@
 import { h, render } from 'preact';
 import Chat from './chat';
+import * as store from 'store'
 
-const userId = Math.random().toString(36).substr(2, 5); // 5 random chars
-const chatId = getUrlParameter('id');
-
-render(<Chat chatId={chatId} userId={userId} />, document.getElementById('intergramChat'));
+render(<Chat chatId={getUrlParameter('id')} userId={getUserId()} />, document.getElementById('intergramChat'));
 
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     let results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+function getUserId () {
+    let userId = store.enabled && store.get('userId');
+
+    if (!userId) {
+        userId = Math.random().toString(36).substr(2, 6); // 6 random chars
+        if (store.enabled) {
+            store.set('userId', userId);
+        }
+    }
+
+    return userId
 }
