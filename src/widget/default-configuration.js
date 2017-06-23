4941a8ecad73c91ca93eeafe9113849a0be7d98b
idoco <ido.c.mail@gmail.com>
2017-06-23 10:22:19 +0300

    make the type "here message" configurable

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -6,6 +6,7 @@ export const defaultConfiguration = {
     autoResponse: 'Looking for the first available admin (It might take a minute)',
     autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
     'contact you, and we will get back to you as soon as we can.',
+    typeHere: 'Type here!',
 
     mainColor: 'rgba(82,179,217,0.9)',
     alwaysUseFloatingButton: false
