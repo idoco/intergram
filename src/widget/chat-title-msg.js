05c154ff17c8b439a240ba124fb95f1dae569e52
Luis Ulloa <timken@gmail.com>
2017-08-07 08:50:40 +0100

    Added text option instead of image url

--- src/widget/chat-title-msg.js
+++ src/widget/chat-title-msg.js
@@ -65,6 +65,7 @@ export default class ChatTitleMsg extends Component {
                             <path d="M20 21.859c0 2.281-1.5 4.141-3.328 4.141h-13.344c-1.828 0-3.328-1.859-3.328-4.141 0-4.109 1.016-8.859 5.109-8.859 1.266 1.234 2.984 2 4.891 2s3.625-0.766 4.891-2c4.094 0 5.109 4.75 5.109 8.859zM16 8c0 3.313-2.688 6-6 6s-6-2.688-6-6 2.688-6 6-6 6 2.688 6 6z"></path>
                         </svg>
                         :
+                        ((conf.closedChatAvatarUrl.indexOf('/')!==-1) ?
                         <img 
                             src={conf.closedChatAvatarUrl} 
                             style={{
@@ -72,7 +73,7 @@ export default class ChatTitleMsg extends Component {
                                 height: 'auto',
                                 borderRadius: '999px'
                             }}
-                        />
+                        />: <center><br/>{conf.closedChatAvatarUrl}</center>)
                     }
                     { (conf.introMessage) ?
                     <div
