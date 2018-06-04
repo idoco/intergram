340945d0601376f1c0db72f6a7c98462eb0dd4a9
Tupax <tupaxmaro@gmail.com>
2018-06-04 16:33:42 -0300

    logo

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -3,12 +3,12 @@ export const defaultConfiguration = {
     titleClosed: 'Chat me!',
     titleOpen: 'Om',
     closedStyle: 'chat', // button or chat
-    closedChatAvatarUrl: 'media/moaks_avatar.png', // only used if closedStyle is set to 'chat'
+    closedChatAvatarUrl: 'media/webclip.jpg', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! IM MOAKS',
+    introMessage: 'Hello! We are Hypermove',
     autoResponse: 'Ok, but IM MOAKS',
     autoNoResponse: 'No one response...maybe there are in yoga class',
-    placeholderText: 'Send a message...',
+    placeholderText: 'Chat with us',
     displayMessageTime: true,
     mainColor: '#1f8ceb',
     alwaysUseFloatingButton: false,
