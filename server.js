59f94fc27fbb79c8f19a2b10dd8f61dd40ae2f14
yamaha252 <mail@yamaha252.ru>
2018-12-04 18:42:40 +0500

    no demo page as index

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('dist', {index: 'demo.html', maxage: '4h'}));
+app.use(express.static('dist'));
 app.use(bodyParser.json());
 
 // handle admin Telegram messages
