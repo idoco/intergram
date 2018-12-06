fad3001b005a9a4543f65259631e31c526705819
yamaha252 <mail@yamaha252.ru>
2018-12-06 23:00:26 +0500

    empty banner if it's hidden

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -41,11 +41,12 @@ export default class Chat extends Component {
                        ref={(input) => { this.input = input }}
                        onKeyPress={this.handleKeyPress}/>
                 {
-                    this.props.conf.displayBanner ? (
+                    this.props.conf.displayBanner ?
                         <a class="banner" href="https://github.com/idoco/intergram" target="_blank">
                             Powered by <b>Intergram</b>&nbsp;
                         </a>
-                    ) : ''
+                        :
+                        <div class="banner" />
                 }
             </div>
         );
