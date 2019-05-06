2a29eb637a7f51e212acf4eb90643e4197f87cc4
Danny Allen <me@dannya.com>
2019-05-06 18:52:38 +0200

    Allow text color of Chat button to be configured via window.intergramCustomizations object, rather than being fixed to #ffffff.
    This allows for the use of more background colors for the Chat button that would otherwise not have sufficient contrast with white text.

--- src/widget/chat-title-msg.js
+++ src/widget/chat-title-msg.js
@@ -5,12 +5,12 @@ export default class ChatTitleMsg extends Component {
     render({isOpened, conf},{}) {
         return (
             <div style={{position: 'relative', cursor: 'pointer'}} onClick={this.props.onClick}>
-                <div 
+                <div
                     className="desktop-closed-message"
                     style={{
                         background: conf.mainColor,
+                        color: conf.textColor,
                         letterSpacing: '1px',
-                        color: '#fff',
                         display: 'block',
                         position: 'absolute',
                         fontSize: '14px',
@@ -26,7 +26,7 @@ export default class ChatTitleMsg extends Component {
                     <div
                         style={{
                             width: 0,
-                            height: 0, 
+                            height: 0,
                             position: 'absolute',
                             top: '12px',
                             right: '-10px',
@@ -36,7 +36,7 @@ export default class ChatTitleMsg extends Component {
                         }}
                     />
                 </div>
-                <div 
+                <div
                     className="desktop-closed-message-avatar"
                     style={{
                         background: '#fff',
@@ -62,8 +62,8 @@ export default class ChatTitleMsg extends Component {
                             <path d="M20 21.859c0 2.281-1.5 4.141-3.328 4.141h-13.344c-1.828 0-3.328-1.859-3.328-4.141 0-4.109 1.016-8.859 5.109-8.859 1.266 1.234 2.984 2 4.891 2s3.625-0.766 4.891-2c4.094 0 5.109 4.75 5.109 8.859zM16 8c0 3.313-2.688 6-6 6s-6-2.688-6-6 2.688-6 6-6 6 2.688 6 6z"></path>
                         </svg>
                         :
-                        <img 
-                            src={conf.closedChatAvatarUrl} 
+                        <img
+                            src={conf.closedChatAvatarUrl}
                             alt="Avatar"
                             style={{
                                 width: '100%',
