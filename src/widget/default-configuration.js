e29037392895f853e8a3c56e72d3e7a0f460e488
Tupax <tupaxmaro@gmail.com>
2019-07-23 11:19:49 -0300

    cambios menores

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,17 +1,16 @@
-
 export const defaultConfiguration = {
-    titleClosed: 'Chat me!',
-    titleOpen: 'Om',
-    closedStyle: 'chat', // button or chat
-    closedChatAvatarUrl: 'media/webclip.jpg', // only used if closedStyle is set to 'chat'
-    cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! We are Hypermove',
-    autoResponse: 'Ok, but IM MOAKS',
-    autoNoResponse: 'No one response...maybe there are in yoga class',
-    placeholderText: 'Chat with us',
-    displayMessageTime: true,
-    mainColor: '#1f8ceb',
-    alwaysUseFloatingButton: false,
-    desktopHeight: 450,
-    desktopWidth: 370
+  titleClosed: "Chat me!",
+  titleOpen: "Weik House",
+  closedStyle: "button", // button or chat
+  closedChatAvatarUrl: "media/webclip.jpg", // only used if closedStyle is set to 'chat'
+  cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
+  introMessage: "Hello! We are Hypermove",
+  autoResponse: "Hola! En que te puedo ayudar?",
+  autoNoResponse: "No one response...maybe there are in yoga class",
+  placeholderText: "Chat with us",
+  displayMessageTime: true,
+  mainColor: "#1f8ceb",
+  alwaysUseFloatingButton: false,
+  desktopHeight: 450,
+  desktopWidth: 370
 };
