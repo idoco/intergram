2b979811983e5cb272a8f3020e03ebe7c66e1604
James Ivings <james@squarecat.io>
2019-09-11 13:59:00 +0800

    feat: added auto messages

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -1,21 +1,19 @@
-import * as store from 'store';
+import * as store from "store";
 
-import { Component, h } from 'preact';
-import { danielleImg, jamesImg } from './admin-images';
+import { Component, h } from "preact";
+import { danielleImg, jamesImg } from "./admin-images";
 
-import MessageArea from './message-area';
-import io from 'socket.io-client';
+import MessageArea from "./message-area";
+import io from "socket.io-client";
 
 export default class Chat extends Component {
-  autoResponseState = 'pristine'; // pristine, set or canceled
+  autoResponseState = "pristine"; // pristine, set or canceled
   autoResponseTimer = 0;
 
   constructor(props) {
     super(props);
     if (store.enabled) {
-      this.messagesKey = `messages.${props.chatId}.${props.host}.${
-        props.userId
-      }`;
+      this.messagesKey = `messages.${props.chatId}.${props.host}.${props.userId}`;
       if (props.conf.private) {
         this.messagesKey = `${this.messagesKey}.private`;
       }
@@ -28,9 +26,9 @@ export default class Chat extends Component {
 
   componentDidMount() {
     this.socket = io.connect();
-    const oldId = store.get('oldId');
-    this.socket.on('connect', () => {
-      this.socket.emit('register', {
+    const oldId = store.get("oldId");
+    this.socket.on("connect", () => {
+      this.socket.emit("register", {
         chatId: this.props.chatId,
         userId: this.props.userId,
         isNewUser: this.props.isNewUser,
@@ -39,29 +37,43 @@ export default class Chat extends Component {
         oldId
       });
     });
-    store.set('oldId', null);
+    store.set("oldId", null);
     this.socket.on(
-      this.props.chatId + '-' + this.props.userId,
+      this.props.chatId + "-" + this.props.userId,
       this.incomingMessage
     );
 
     if (!this.state.messages.length) {
       this.writeToMessages({
         text: this.props.conf.introMessage,
-        from: 'admin'
+        from: "admin"
       });
     }
   }
 
   render({}, state) {
+    const now = new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Singapore"
+    });
+    let hours = new Date(now).getHours();
+    let minutes = new Date(now).getMinutes();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    const ampm = hours > 12 ? "pm" : "am";
     return (
       <div class="chat-container">
         <div class="chat-header">
           <h5>Questions? Problems? Chat with us!</h5>
           <p>
-            If we're awake then we'll typically respond to your message within a
-            few minutes.
+            It's currently {`${hours}:${minutes}${ampm}`} where we are, if we're
+            awake then we'll typically respond to your message within a few
+            minutes.
           </p>
+
           <div class="admin-images">
             <img src={danielleImg} alt="danielle-img" />
             <img src={jamesImg} alt="james-img" />
@@ -78,54 +90,59 @@ export default class Chat extends Component {
           }}
           onKeyPress={this.handleKeyPress}
         />
-
-        {/* <a
-          class="banner"
-          href="https://github.com/idoco/intergram"
-          target="_blank"
-        >
-          Powered by <b>Intergram</b>&nbsp;
-        </a> */}
       </div>
     );
   }
 
   handleKeyPress = e => {
-    this.socket.send({ action: 'typing' });
+    let message;
+    let timeout;
+    const now = new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Singapore"
+    });
+    let hours = new Date(now).getHours();
+    const isNightTime = hours < 8 && hours > 11;
+    this.socket.send({ action: "typing" });
     if (e.keyCode == 13 && this.input.value) {
       let text = this.input.value;
       this.socket.send({
-        action: 'message',
+        action: "message",
         msg: {
           text,
-          from: 'visitor'
+          from: "visitor"
         },
         userData: this.props.conf.userData
       });
-      this.input.value = '';
-
-      if (this.autoResponseState === 'pristine') {
+      this.input.value = "";
+      if (this.autoResponseState === "pristine") {
+        if (isNightTime) {
+          timeout = 1000;
+          message = `(Auto message) It's night time right now and we're probably asleep, please leave your email so we can contact you later, or fill out the [form here](https://leavemealone.app/feedback).`;
+        } else {
+          timeout = 60 * 1000 * 2;
+          message = `(Auto message) We're not around right now, please leave your email so we can contact you later, or fill out the [form here](https://leavemealone.app/feedback).`;
+        }
         this.autoResponseTimer = setTimeout(() => {
-          // this.writeToMessages({
-          //   text: this.props.conf.autoNoResponse,
-          //   from: 'admin'
-          // })
-          this.autoResponseState = 'canceled';
-        }, 60 * 1000 * 3);
-        this.autoResponseState = 'set';
+          this.writeToMessages({
+            text: message,
+            from: "admin"
+          });
+          this.autoResponseState = "canceled";
+        }, timeout); // 2 MINUTES
+        this.autoResponseState = "set";
       }
     }
   };
 
   incomingMessage = msg => {
     this.writeToMessages(msg);
-    if (msg.from === 'admin') {
-      document.getElementById('messageSound').play();
+    if (msg.from === "admin") {
+      document.getElementById("messageSound").play();
 
-      if (this.autoResponseState === 'pristine') {
-        this.autoResponseState = 'canceled';
-      } else if (this.autoResponseState === 'set') {
-        this.autoResponseState = 'canceled';
+      if (this.autoResponseState === "pristine") {
+        this.autoResponseState = "canceled";
+      } else if (this.autoResponseState === "set") {
+        this.autoResponseState = "canceled";
         clearTimeout(this.autoResponseTimer);
       }
     }
@@ -152,7 +169,7 @@ export default class Chat extends Component {
           messages.push(msg);
         });
       } catch (e) {
-        console.log('failed to add new message to local storage', e);
+        console.log("failed to add new message to local storage", e);
         store.set(this.messagesKey, []);
       }
     }
--- src/chat/message-area.js
+++ src/chat/message-area.js
@@ -1,58 +1,62 @@
-import { Component, h } from 'preact'
-import { danielleImg, jamesImg } from './admin-images'
+import { Component, h } from "preact";
+import { danielleImg, jamesImg } from "./admin-images";
 
-import dateFormat from 'dateformat'
+import MarkdownIt from "markdown-it";
+import dateFormat from "dateformat";
 
-const dayInMillis = 60 * 60 * 24 * 1000
+const md = new MarkdownIt();
+const dayInMillis = 60 * 60 * 24 * 1000;
 
 const admins = {
   jivings: jamesImg,
   dinkydani: danielleImg
-}
+};
 export default class MessageArea extends Component {
-  componentDidMount () {
-    this.ref.scrollTo(0, this.ref.scrollHeight)
+  componentDidMount() {
+    this.ref.scrollTo(0, this.ref.scrollHeight);
   }
 
-  componentDidUpdate () {
-    this.ref.scrollTo(0, this.ref.scrollHeight)
+  componentDidUpdate() {
+    this.ref.scrollTo(0, this.ref.scrollHeight);
   }
 
-  render (props, {}) {
-    const currentTime = new Date()
+  render(props, {}) {
+    const currentTime = new Date();
     return (
       <ol
-        class='chat'
+        class="chat"
         ref={r => {
-          this.ref = r
+          this.ref = r;
         }}
       >
         {props.messages.map(({ name, text, from, time, adminName }) => {
-          const isAdmin = from !== 'visitor'
+          const renderedMsg = md.render(text);
+          console.log(renderedMsg);
+          const isAdmin = from !== "visitor";
           return (
-            <li class={isAdmin ? 'admin' : 'visitor'}>
+            <li class={isAdmin ? "admin" : "visitor"}>
               {admins[adminName] ? (
-                <div class='msg-image'>
-                  <img src={admins[adminName]} alt='admin-image' />
+                <div class="msg-image">
+                  <img src={admins[adminName]} alt="admin-image" />
                 </div>
               ) : null}
 
-              <div class='msg'>
-                <p>{text}</p>
+              <div class="msg">
+                <div dangerouslySetInnerHTML={{ __html: renderedMsg }} />
                 {props.conf.displayMessageTime ? (
-                  <div class='time'>
+                  <div class="time">
                     {currentTime - new Date(time) < dayInMillis
-                      ? dateFormat(time, 'HH:MM')
-                      : dateFormat(time, 'm/d/yy HH:MM')}
+                      ? dateFormat(time, "HH:MM")
+                      : dateFormat(time, "m/d/yy HH:MM")}
                   </div>
                 ) : (
-                  ''
+                  ""
                 )}
               </div>
             </li>
-          )
+          );
         })}
       </ol>
-    )
+    );
   }
 }
